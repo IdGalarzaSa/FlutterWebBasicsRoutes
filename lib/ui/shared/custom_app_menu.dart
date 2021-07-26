fc4f24facfd78e87b3af89ad6e73984f76d44f37
Ivan Galarza <igalarza@bnb.com.bo>
2021-07-26 00:26:11 -0400

    responsive menu

--- lib/ui/shared/custom_app_menu.dart
+++ lib/ui/shared/custom_app_menu.dart
@@ -8,6 +8,17 @@ import 'package:flutter_web_basics/ui/views/counter_stateful_view.dart';
 class CustomAppMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (_, BoxConstraints constraints) {
+        return constraints.maxWidth > 520 ? _HorizontalMenu() : _VerticalMenu();
+      },
+    );
+  }
+}
+
+class _HorizontalMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Container(
       color: Colors.blueGrey,
       child: Row(
@@ -33,6 +44,55 @@ class CustomAppMenu extends StatelessWidget {
           ),
           SizedBox(width: 10)
         ],
+      ),
+    );
+  }
+}
+
+class _VerticalMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blueGrey,
+      child: Container(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              child: CustomTextButton(
+                buttonFunction: () =>
+                    // Navigator.pushNamed(context, CounterStatefulView.ROUTE_NAME),
+                    locator<NavigationService>()
+                        .navigateTo(CounterStatefulView.ROUTE_NAME),
+                buttonText: "Stateful counter",
+                textAlignment: Alignment.centerLeft,
+              ),
+            ),
+            SizedBox(width: 10),
+            Container(
+              width: double.infinity,
+              child: CustomTextButton(
+                buttonFunction: () => locator<NavigationService>()
+                    .navigateTo(CounterProviderView.ROUTE_NAME),
+                buttonText: "Provider counter",
+                textAlignment: Alignment.centerLeft,
+              ),
+            ),
+            SizedBox(width: 10),
+            Container(
+              width: double.infinity,
+              child: CustomTextButton(
+                buttonFunction: () =>
+                    locator<NavigationService>().navigateTo("/ddasda1232132"),
+                buttonText: "Other page",
+                textAlignment: Alignment.centerLeft,
+              ),
+            ),
+            SizedBox(width: 10)
+          ],
+        ),
       ),
     );
   }
--- lib/ui/shared/custom_text_button.dart
+++ lib/ui/shared/custom_text_button.dart
@@ -4,12 +4,14 @@ class CustomTextButton extends StatelessWidget {
   final Function buttonFunction;
   final String buttonText;
   final Color buttonColor;
+  final AlignmentGeometry textAlignment;
 
   const CustomTextButton({
     Key? key,
     required this.buttonFunction,
     required this.buttonText,
     this.buttonColor = Colors.amber,
+    this.textAlignment = Alignment.center,
   }) : super(key: key);
 
   @override
@@ -17,6 +19,7 @@ class CustomTextButton extends StatelessWidget {
     return TextButton(
       style: TextButton.styleFrom(
         primary: this.buttonColor,
+        alignment: textAlignment,
       ),
       onPressed: () => this.buttonFunction(),
       child: Padding(padding: EdgeInsets.all(10), child: Text(this.buttonText)),
